150f2bafe0de361e073d88d8afefff4f42b3a87f
Florence <plorenznl@gmail.com>
2024-11-30 19:34:20 +0800

    Define the Function to Write Data to a File Using open()

--- user_info_collector_editor.py
+++ user_info_collector_editor.py
@@ -5,11 +5,8 @@ def personal_info(data):
         user_info.append(ask_user if ask_user else "N/A") # Append the input or "N/A" if blank
     return user_info
 
+def write_info(people, data, filename="info_file.txt"):
+    collected_info = open("./info_file.txt", "w") # Open a text file in write mode
 
 
-
-
-collected_info = open("./info_file.txt", "w")
-
-
-collected_info.close()
+    collected_info.close()
