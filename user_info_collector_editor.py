16856d8a9e1343a2f82871de3653b9f0fa5b8cda
Florence <plorenznl@gmail.com>
2024-11-30 20:18:35 +0800

    For Loop

--- user_info_collector_editor.py
+++ user_info_collector_editor.py
@@ -4,7 +4,7 @@ def personal_info(data):
     for info in data: # Loop through each info in the data list
         ask_user = input(f"{info}: ") # Ask user for input
         if ask_user:
-            user_info.appent(ask_user)
+            user_info.append(ask_user)
         else:
             user_info.append("N/A") # Append the input or "N/A" if blank
     return user_info
@@ -12,8 +12,8 @@ def personal_info(data):
 def write_info(people, data, filename="info_file.txt"):
     collected_info = open("./info_file.txt", "w") # Open a text file in write mode
 
-    for person in people:
-        for info in range(len(data)):
+    for person in people: 
+        for info_index in range(len(data)):
+            collected_info.write(f"{data[info_index]}: {person[info_index]}\n")
 
-
-            collected_info.close()
+    collected_info.close()
