77e940631258de7bb2c064e956163e3150cdb8b1
Florence <plorenznl@gmail.com>
2024-11-30 19:04:04 +0800

    Define the Function to Collect Information

--- user_info_collector_editor.py
+++ user_info_collector_editor.py
@@ -1,3 +1,15 @@
+def personal_info(data):
+    user_info = [] # This will store the data for one person
+    for info in data: # Loop through each info in the data list
+        ask_user = input(f"{info}: ") # Ask user for input
+        user_info.append(ask_user if ask_user else "N/A") # Append the input or "N/A" if blank
+    return user_info
+
+
+
+
+
 collected_info = open("./info_file.txt", "w")
+
 
 collected_info.close()
