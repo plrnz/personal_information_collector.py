6ead4afa7281ea930d59aa50e9bc24226a56034a
Florence <plorenznl@gmail.com>
2024-11-30 19:51:06 +0800

    Simple Modifications

--- user_info_collector_editor.py
+++ user_info_collector_editor.py
@@ -1,12 +1,19 @@
 def personal_info(data):
     user_info = [] # This will store the data for one person
+    
     for info in data: # Loop through each info in the data list
         ask_user = input(f"{info}: ") # Ask user for input
-        user_info.append(ask_user if ask_user else "N/A") # Append the input or "N/A" if blank
+        if ask_user:
+            user_info.appent(ask_user)
+        else:
+            user_info.append("N/A") # Append the input or "N/A" if blank
     return user_info
 
 def write_info(people, data, filename="info_file.txt"):
     collected_info = open("./info_file.txt", "w") # Open a text file in write mode
 
+    for person in people:
+        for info in range(len(data)):
 
-    collected_info.close()
+
+            collected_info.close()
