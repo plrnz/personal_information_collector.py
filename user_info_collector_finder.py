edefa1e2c93cc4115720108d7f7e67ea2019a393
Florence <plorenznl@gmail.com>
2024-12-01 18:56:30 +0800

    Ask for input

--- user_info_collector_finder.py
+++ user_info_collector_finder.py
@@ -1,3 +1,4 @@
 with open("./info_file.txt", "r") as stored_data:
     line = stored_data.readlines()
-    
+
+find_name = input("Please enter the name of the person that you want to have the information: ")
