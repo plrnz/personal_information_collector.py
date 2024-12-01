53504890c25b7b6e2b448bd984aee81be5d6a094
Florence <plorenznl@gmail.com>
2024-12-01 14:19:01 +0800

    For Loop, If-Else, Entry Separator, Save Data, Clear Data, Add Line

--- user_info_collector_reader.py
+++ user_info_collector_reader.py
@@ -4,3 +4,11 @@ with open("./info_file.txt", "r") as stored_info:
 
 data_info = []
 one_person_info = [] 
+
+for line in lines:
+    if "-" * 40 in line:
+        if one_person_info:
+            data_info.append(one_person_info)
+        one_person_info = []
+    else:
+        one_person_info.append(line.strip())
