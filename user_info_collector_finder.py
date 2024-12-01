b1ecdacea49d209ee0121bc0f7ee2c7fbe141f63
Florence <plorenznl@gmail.com>
2024-12-01 19:21:35 +0800

    Info Finder

--- user_info_collector_finder.py
+++ user_info_collector_finder.py
@@ -1,4 +1,12 @@
 with open("./info_file.txt", "r") as stored_data:
-    line = stored_data.readlines()
+    data_info = stored_data.read().split("-" * 20)
 
-find_name = input("Please enter the name of the person that you want to have the information: ")
+find_name = input("Please enter the name of the person that you want to have the information: ").strip()
+
+for entry in data_info:
+    if find_name in entry:
+        print("Information found.\n")
+        print(entry.strip())
+        break
+else:
+        print("\nThe person's information can't be found.")
